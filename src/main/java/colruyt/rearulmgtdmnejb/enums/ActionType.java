0412966db8c0795c4a97a415c338e4ff04bb78c5
Satish Kumar Thangavelu <satishkumar.thangavelu@colruytgroup.com>
2019-05-22 17:37:54 +0200

    Modifying the dicount Euro Action type Id

--- src/main/java/colruyt/rearulmgtdmnejb/enums/ActionType.java
+++ src/main/java/colruyt/rearulmgtdmnejb/enums/ActionType.java
@@ -2,19 +2,19 @@ package colruyt.rearulmgtdmnejb.enums;
 
 public enum ActionType {
 		PRICE_PROMO(1,"Price Promo", 1L),
-		DISCOUNT_IN_PERCENT(2,"Discount %",  2L),
-		DISCOUNT_IN_EURO(3,"Discount Euro", 3L),
-		XNTH_FOR_Y_PERCENT_OFF(5," Xnth for Y% off",  5L),
-		POINTS(6,"Points",  7L),
-		STAMPS(7, "Stamps",  6L),
+		DISCOUNT_IN_PERCENT(3,"Discount %",  2L),
+		DISCOUNT_IN_EURO(2,"Discount Euro", 3L),
 		X_PLUS_Y(11,"X+Y", 4L),
+		XNTH_FOR_Y_PERCENT_OFF(5," Xnth for Y% off",  5L),
+		STAMPS(7, "Stamps",  6L),
+		POINTS(6,"Points",  7L),
 		GADGET_OR_PREMIUM(12,"Gadget/Premium", 8L),
-		OTHER_FREE_PRODUCTS(13,"Other Free Products", 10L),
 		CASHBACK(14,"Cashback",  9L),
+		OTHER_FREE_PRODUCTS(13,"Other Free Products", 10L),
 		BON_ON_PACK(15,"Bon on pack", 11L),	
-		FREE_CONTENT(16,"Free Content",16L),
 		ALL(40, "All", 14L),
-		NONE(50, "None", 15L);
+		NONE(50, "None", 15L),
+		FREE_CONTENT(16,"Free Content",16L);
 
 	private int actionTypeId;
 	private String actionTypeValue;
